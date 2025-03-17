69027352ccf19fd2e33ee7bd43d0b4f6c5b2e864
Kellia855 <k.kamikazi@alustudent.com>
2025-03-17 12:21:13 +0200

    classes

---
+++ TheCall.py
@@ -0,0 +1,9 @@
+from Laptop_class import Laptops
+
+# def Laptop(500,"x64",Asus)
+Laptop1 = Laptops(500,"x64","Asus")
+print(Laptop1.Model)
+print(Laptop1.Architecture)
+print(Laptop1.HardDisk)
+Laptop1.TurnOn()
+Laptop1.bits()
